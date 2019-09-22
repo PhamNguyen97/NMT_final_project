bb47d531ca69341b7144759d244676cc4756f498
PhamNguyen197 <quocnguyenkh@gmail.com>
2019-09-22 07:48:00 +0700

    train same as test

--- model/decoder.py
+++ model/decoder.py
@@ -76,6 +76,7 @@ class Decoder(tf.keras.Model):
         # for _ in range(self.max_length-1):
         for current_word_index in range(inputs.shape[1]):
             current_word = inputs[:,current_word_index]
+            current_word = tf.expand_dims(1)
             print(inputs.shape, current_word.shape)
             all_state = self.embedding(current_word)
             current_initial_state = []
